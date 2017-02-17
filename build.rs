0ddf7a3994d22a8aaf62fdbfa4ca0c8f9181399d
Simonas Kazlauskas <git@kazlauskas.me>
2017-02-18 01:02:50 +0200

    Initial commit

---
+++ build.rs
@@ -0,0 +1,17 @@
+extern crate gcc;
+
+fn main() {
+    let sources = ::std::fs::read_dir("src")
+        .expect("listable source directory")
+        .map(|de| de.expect("file in directory").path())
+        .filter(|p| p.extension().map(|ext| ext == "cpp") == Some(true))
+        .collect::<Vec<_>>();
+    let mut config = gcc::Config::new();
+    for source in sources.iter() {
+        config.file(source.to_str().unwrap());
+    }
+    config.flag("-std=c++11");
+    config.flag("-fno-omit-frame-pointer");
+    config.cpp(true);
+    config.compile("libfuzzer.a");
+}
