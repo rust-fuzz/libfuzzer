f01cb566be4b86284dfcab453e6266cb1b125e04
Frederik Deweerdt <fdeweerdt@fastly.com>
2017-06-08 16:45:19 -0700

    Optionally use a custom libFuzzer.a archive
    Tweak build.rs so that it's possible to use a custom libFuzzer.a
    archive. This is necessary when using OSS-Fuzz
    (https://github.com/google/oss-fuzz): the build
    system has to link with the libFuzzer provided by
    the `LIB_FUZZING_ENGINE` environment variable (see
    https://github.com/google/oss-fuzz/blob/master/docs/new_project_guide.md)

--- build.rs
+++ build.rs
@@ -1,17 +1,30 @@
 extern crate gcc;
 
 fn main() {
-    let sources = ::std::fs::read_dir("llvm/lib/Fuzzer")
-        .expect("listable source directory")
-        .map(|de| de.expect("file in directory").path())
-        .filter(|p| p.extension().map(|ext| ext == "cpp") == Some(true))
-        .collect::<Vec<_>>();
-    let mut config = gcc::Config::new();
-    for source in sources.iter() {
-        config.file(source.to_str().unwrap());
+    if let Ok(custom) = ::std::env::var("CUSTOM_LIBFUZZER_PATH") {
+        let custom_lib_dir = ::std::path::PathBuf::from(&custom);
+        let custom_lib_dir = custom_lib_dir.parent().unwrap().to_string_lossy();
+
+        let custom_lib_name = ::std::path::PathBuf::from(&custom);
+        let custom_lib_name = custom_lib_name.file_stem().unwrap().to_string_lossy();
+        let custom_lib_name = &custom_lib_name[3..];
+
+        println!("cargo:rustc-link-search=native={}", custom_lib_dir);
+        println!("cargo:rustc-link-lib=static={}", custom_lib_name);
+        println!("cargo:rustc-link-lib=stdc++");
+    } else {
+        let mut config = gcc::Config::new();
+        let sources = ::std::fs::read_dir("llvm/lib/Fuzzer")
+            .expect("listable source directory")
+            .map(|de| de.expect("file in directory").path())
+            .filter(|p| p.extension().map(|ext| ext == "cpp") == Some(true))
+            .collect::<Vec<_>>();
+        for source in sources.iter() {
+            config.file(source.to_str().unwrap());
+        }
+        config.flag("-std=c++11");
+        config.flag("-fno-omit-frame-pointer");
+        config.cpp(true);
+        config.compile("libfuzzer.a");
     }
-    config.flag("-std=c++11");
-    config.flag("-fno-omit-frame-pointer");
-    config.cpp(true);
-    config.compile("libfuzzer.a");
 }
