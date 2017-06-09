9ba19053e78328bf77321b89ae7258dad0cb7121
Frederik Deweerdt <fdeweerdt@fastly.com>
2017-06-09 11:24:02 -0700

    Optionally use a custom libFuzzer.a archive
    - only use one PathBuf
    - only remove the 'lib' prefix if present
    - Add a note saying we're assuming c++ linkage

--- build.rs
+++ build.rs
@@ -2,15 +2,15 @@ extern crate gcc;
 
 fn main() {
     if let Ok(custom) = ::std::env::var("CUSTOM_LIBFUZZER_PATH") {
-        let custom_lib_dir = ::std::path::PathBuf::from(&custom);
-        let custom_lib_dir = custom_lib_dir.parent().unwrap().to_string_lossy();
+        let custom_lib_path = ::std::path::PathBuf::from(&custom);
+        let custom_lib_dir = custom_lib_path.parent().unwrap().to_string_lossy();
 
-        let custom_lib_name = ::std::path::PathBuf::from(&custom);
-        let custom_lib_name = custom_lib_name.file_stem().unwrap().to_string_lossy();
-        let custom_lib_name = &custom_lib_name[3..];
+        let custom_lib_name = custom_lib_path.file_stem().unwrap().to_string_lossy();
+        let custom_lib_name = custom_lib_name.trim_left_matches("lib");
 
         println!("cargo:rustc-link-search=native={}", custom_lib_dir);
         println!("cargo:rustc-link-lib=static={}", custom_lib_name);
+        /* FIXME: this is assuming a C++ fuzzer, but should be customizable */
         println!("cargo:rustc-link-lib=stdc++");
     } else {
         let mut config = gcc::Config::new();
