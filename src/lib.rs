05b9ffcdb83061341cd92f4b678ec770b2f6b5b0
Paul Grandperrin <paul.grandperrin@gmail.com>
2018-04-24 12:32:07 +0200

    Register a custom panic hook calling process::abort()
    see the rational at https://github.com/rust-fuzz/honggfuzz-rs/commit/abe2b4c93121e6fdca498e7e1645f63c9d41369d
    
    closes #31

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,23 @@ pub fn test_input_wrap(data: *const u8, size: usize) -> i32 {
     ::std::panic::catch_unwind(|| unsafe {
         let data_slice = ::std::slice::from_raw_parts(data, size);
         rust_fuzzer_test_input(data_slice);
-    }).err().map(|_| ::std::process::abort());
+    })
+    .err().map(|_|
+        // hopefully the custom panic hook will be called before and abort the
+        // process before the stack frames are unwinded.
+        ::std::process::abort()
+    );
+    0
+}
+
+#[export_name="LLVMFuzzerInitialize"]
+pub fn initialize(_argc: *const isize, _argv: *const *const *const u8) -> isize {
+    // Registers a panic hook that aborts the process before unwinding.
+    // It is useful to abort before unwinding so that the fuzzer will then be
+    // able to analyse the process stack frames to tell different bugs appart.
+    ::std::panic::set_hook(Box::new(|_| {
+            ::std::process::abort();
+    }));
     0
 }
 
