e6a18755491e926b247afbb96c803aa89baa6ace
Paul Grandperrin <paul.grandperrin@gmail.com>
2018-04-28 16:27:08 +0200

    Change API to be the same as Honggfuzz-rs and eventually AFL.rs
    closes rust-fuzz/cargo-fuzz#119
    closes rust-fuzz/cargo-fuzz#101

--- example_arbitrary/src/main.rs
+++ example_arbitrary/src/main.rs
@@ -1,10 +1,19 @@
-#![no_main]
-
 #[macro_use]
 extern crate libfuzzer_sys;
+extern crate arbitrary;
 
-fuzz_target!(|data: u16| {
-    if data == 0xba7 { // ba[nana]
+fn main() {
+    // Here you can parse `std::env::args and
+    // setup / initialize your project
+
+    // The fuzz macro gives an arbitrary object (see `arbitrary crate`)
+    // to a closure-like block of code.
+    // For performance, it is recommended that you use the native type
+    // `&[u8]` when possible.
+    // Here, this slice will contain a "random" quantity of "random" data.
+    fuzz!(|data: u16| {
+        if data == 0xba7 { // ba[nana]
         panic!("success!");
-    }
-});
+        }
+    });
+}
