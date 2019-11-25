628032ae523f9d1aaaabcc5b16ea2f445be0fbb9
koushiro <koushiro.cqx@gmail.com>
2019-11-25 17:00:06 +0800

    Update arbitrary
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,24 @@
-extern "C" {
-    #![allow(improper_ctypes)] // we do not actually cross the FFI bound here
+#![allow(improper_ctypes)] // we do not actually cross the FFI bound here
 
+extern "C" {
     fn rust_fuzzer_test_input(input: &[u8]);
 }
 
-#[export_name="LLVMFuzzerTestOneInput"]
+#[export_name = "LLVMFuzzerTestOneInput"]
 pub fn test_input_wrap(data: *const u8, size: usize) -> i32 {
-    ::std::panic::catch_unwind(|| unsafe {
+    let test_input = ::std::panic::catch_unwind(|| unsafe {
         let data_slice = ::std::slice::from_raw_parts(data, size);
         rust_fuzzer_test_input(data_slice);
-    })
-    .err().map(|_|
+    });
+    if test_input.err().is_some() {
         // hopefully the custom panic hook will be called before and abort the
         // process before the stack frames are unwinded.
-        ::std::process::abort()
-    );
+        ::std::process::abort();
+    }
     0
 }
 
-#[export_name="LLVMFuzzerInitialize"]
+#[export_name = "LLVMFuzzerInitialize"]
 pub fn initialize(_argc: *const isize, _argv: *const *const *const u8) -> isize {
     // Registers a panic hook that aborts the process before unwinding.
     // It is useful to abort before unwinding so that the fuzzer will then be
@@ -29,7 +29,7 @@ pub fn initialize(_argc: *const isize, _argv: *const *const *const u8) -> isize 
     // We will be able to remove this code when
     // https://github.com/rust-lang/cargo/issues/5423 is fixed.
     ::std::panic::set_hook(Box::new(|_| {
-            ::std::process::abort();
+        ::std::process::abort();
     }));
     0
 }
@@ -38,7 +38,7 @@ pub fn initialize(_argc: *const isize, _argv: *const *const *const u8) -> isize 
 macro_rules! fuzz_target {
     (|$bytes:ident| $body:block) => {
         #[no_mangle]
-        pub extern fn rust_fuzzer_test_input($bytes: &[u8]) {
+        pub extern "C" fn rust_fuzzer_test_input($bytes: &[u8]) {
             $body
         }
     };
@@ -47,7 +47,7 @@ macro_rules! fuzz_target {
     };
     (|$data:ident: $dty: ty| $body:block) => {
         #[no_mangle]
-        pub extern fn rust_fuzzer_test_input(bytes: &[u8]) {
+        pub extern "C" fn rust_fuzzer_test_input(bytes: &[u8]) {
             use arbitrary::{Arbitrary, RingBuffer};
 
             let mut buf = match RingBuffer::new(bytes, bytes.len()) {
