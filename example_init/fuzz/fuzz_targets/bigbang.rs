e79341396604dbf913a0a25402c0123e2ab7ddb7
zi0Black <13380579+zi0Black@users.noreply.github.com>
2024-12-28 10:48:02 +0100

    Better example using static

--- example_init/fuzz/fuzz_targets/bigbang.rs
+++ example_init/fuzz/fuzz_targets/bigbang.rs
@@ -6,11 +6,8 @@ fuzz_target!(
     init: {
         // Custom initialization code here
         println!("Initializing fuzzer...");
-        std::env::set_var("MY_FUZZER_INIT", "1337");
+        example_init::initialize();
     },
     |data: &[u8]| {
-    if std::env::var("MY_FUZZER_INIT").unwrap() == "1337" && data == "bigbang!".as_bytes() {
-        panic!("success!");
-    }
     example_init::bigbang(data);
 });
