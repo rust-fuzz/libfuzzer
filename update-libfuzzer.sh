f1282130cfeac789a5cd3e60ac236b9081445f34
R. Elliott Childre <elliottchildre329@gmail.com>
2025-07-03 10:25:31 -0400

    Update vendored LibFuzzer to LLVM 20.x release
    Specifically:
    
    * Tag:    `llvmorg-20.1.7`
    * Commit: `6146a88f60492b520a36f8f8f3231e15f3cc6082`
    
    Major changes:
    
    * Clarified behavior of `-max_len` option
    
    * Avoid UBSan faults in LibFuzzer by guarding `memcpy()` calls to
      check for valid size argument
    
    * Fix building LibFuzzer with `clang-cl`
    
    * Thread name setting logic for Fuchsia and Windows
    
    Full commit log:
    
    `git log --format=ref release/19.x..release/20.x -- 'compiler-rt/lib/fuzzer/'`
    
    ```
    091741a880c2 ([libfuzzer] Clarify -max_len behavior on bigger files (#123095), 2025-01-23)
    f9125ddc1faa (Revert "[libfuzzer] use timer_create() instead of setitimer() for linux" (#115811), 2024-11-11)
    3b29a8a00809 ([libfuzzer] use timer_create() instead of setitimer() for linux (#110274), 2024-11-12)
    5082acce4fd3 ([compiler-rt] Add custom libc++ workaround for CMake < 3.26, 2024-11-10)
    87f4bc0acad6 ([compiler-rt] [fuzzer] Skip trying to set the thread name on MinGW (#115167), 2024-11-07)
    e7bad34475e2 ([compiler-rt] Use installed libc++(abi) for tests instead of build tree, 2024-11-06)
    a6fdfefbd04d ([compiler-rt] Include stdlib.h for exit() (#115025), 2024-11-05)
    d54953ef472b ([fuzzer] fix clang-cl build fuzzer lit test failure (#112339), 2024-10-17)
    b4130bee6bfd (Fix libFuzzer not building with pthreads on Windows (#109525), 2024-09-24)
    b32dc677325c (Revert "[compiler-rt][fuzzer] SetThreadName build fix for Mingwin attempt (#106902)", 2024-09-02)
    7c4cffd9d8be ([compiler-rt][fuzzer] SetThreadName build fix for Mingwin attempt (#106902), 2024-09-01)
    f47966b1de45 ([compiler-rt] Reland "SetThreadName implementation for Fuchsia" (#105179), 2024-08-21)
    ddaa8284f5b4 (Revert "[compiler-rt][fuzzer] implements SetThreadName for fuchsia." (#105162), 2024-08-20)
    31cc4ccdea92 ([compiler-rt][fuzzer] implements SetThreadName for fuchsia. (#99953), 2024-08-20)
    bde4ffe75214 (Don't pass null pointers to memcmp and memcpy in libFuzzer (#96775), 2024-08-13)
    7202fe582931 ([compiler-rt] Silence warnings, 2024-08-11)
    ```

--- update-libfuzzer.sh
+++ update-libfuzzer.sh
@@ -8,7 +8,7 @@ set -ex
 
 # The LLVM commit from which we are vendoring libfuzzer. This must be a commit
 # hash from https://github.com/llvm/llvm-project
-COMMIT=ab51eccf88f5321e7c60591c5546b254b6afab99
+COMMIT=6146a88f60492b520a36f8f8f3231e15f3cc6082
 
 cd "$(dirname $0)"
 project_dir="$(pwd)"
